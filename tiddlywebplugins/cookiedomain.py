2043c81e165b4b249935aeff3d5a9a6a05f0cbb3
Chris Dent <chris.dent@gmail.com>
2011-11-12 17:09:33 +0000

    pylint cleanups, in docs

--- tiddlywebplugins/cookiedomain.py
+++ tiddlywebplugins/cookiedomain.py
@@ -1,5 +1,20 @@
 """
-Description of My Plugin.
+A TiddlyWeb plugin that modifies the tiddlyweb_user cookie to add a domain.
+
+The goal here is to allow auth in one subdomain to be useful
+in another.
+
+When installed as a system_plugin, any 'tiddlyweb_user' cookie
+produced lower down in the stack with a Set-Cookie will have
+a Domain attribute added with a value set from
+
+* config['cookie_domain']
+* config['server_host']['host']
+
+choosing the first if defined.
+
+To install add tiddlywebplugins.cookiedomain to system_plugins
+in tiddlywebconfig.py.
 """
 
 import Cookie
@@ -11,6 +26,10 @@ COOKIE_NAME = 'tiddlyweb_user'
 
 
 class CookieDomain(object):
+    """
+    Class operating as a WSGI callable to set the domain
+    on the tiddlyweb_user cookie if it sees one pass by.
+    """
 
     def __init__(self, application):
         self.application = application
@@ -51,6 +70,9 @@ class CookieDomain(object):
 
 
 def init(config):
+    """
+    Extend server_response_filters to add CookieDomain as a filter.
+    """
     if CookieDomain not in config['server_response_filters']:
         config['server_response_filters'].insert(
                 config['server_response_filters'].index(
